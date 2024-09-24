55a26b4447c08224f4b890d9e31ffbac9ee4c334
ahmedalaadev <ahmedaboalaa012@gmail.com>
2024-09-24 18:58:37 +0300

    Add project files.

---
+++ DVLD/People/ManagePeople.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.People
+{
+    public partial class ManagePeople : Form
+    {
+        public ManagePeople()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Database/ConnectionSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class ConnectionSettings
+    {
+        public static string ConnectionString = "Server=localhost,1433; Database=DVLD; User Id=sa; Password=123456;";
+    }
+}
